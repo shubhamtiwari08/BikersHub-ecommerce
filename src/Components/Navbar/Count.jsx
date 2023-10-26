6a201575fedb0ebb1601c8f90d325c5d6e542d48
shubham tiwari <st1173076@gmail.com>
2023-10-26 13:06:55 +0530

    refactor:changed the ui of navbar

--- src/Components/Navbar/Count.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Navbar.css";
-
-function Count({ count }) {
-  return <div className="count-num">{count}</div>;
-}
-
-export default Count;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,36 +12,31 @@ import {
   faHeart,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
-import Count from "./Count";
+ 
 import { authContext } from "../../Contexts/Auth/AuthContext";
 import { cartContext } from "../../Contexts/Cart/CartContext";
 import { wishlistContext } from "../../Contexts/Wishlist/WishlistContext";
 import { FilterContext } from "../../Contexts/FilterContext/FilterContext";
- 
+
 function Navbar() {
-  const [navToggle,setNavToggle] = useState(false)
+  const [navToggle, setNavToggle] = useState(false);
   const { isLogged, logoutHandler } = useContext(authContext);
-  const { cartState,cartDispatch } = useContext(cartContext);
+  const { cartState, cartDispatch } = useContext(cartContext);
   const { wishlistState, wishlistDispatch } = useContext(wishlistContext);
   const { filterDispatch } = useContext(FilterContext);
 
-
-  const handleLogout =()=>{
-      logoutHandler()
-      wishlistDispatch({type:"CLEAR_WISHLIST"})
-      cartDispatch({type:"CLEAR_CART"})
-
-  }
+  const handleLogout = () => {
+    logoutHandler();
+    wishlistDispatch({ type: "CLEAR_WISHLIST" });
+    cartDispatch({ type: "CLEAR_CART" });
+  };
 
   const Navigate = useNavigate();
 
-  const handleFilterAndNavigation=()=>{
-      setNavToggle(!navToggle)
-      console.log()
-  }
-
-  
-
+  const handleFilterAndNavigation = () => {
+    setNavToggle(!navToggle);
+    console.log();
+  };
 
   return (
     <div className="nav-bar">
@@ -78,79 +73,104 @@ function Navbar() {
             </Link>
           </button>
         )}
-        <FontAwesomeIcon
-          icon={faHeart}
-          color="var(--primary-color)"
-          size="xl"
-          onClick={() => Navigate(`/wishlist`)}
-        />
-        {wishlistState.wishlistCount > 0 && (
-          <Count count={wishlistState.wishlistCount} />
-        )}
-        <FontAwesomeIcon
-          icon={faCartShopping}
-          color="var(--primary-color)"
-          size="lg"
-          onClick={() => Navigate("/cart")}
-        />
-        <div className="cart-count">
-          {" "}
-          {cartState.cartCount > 0 && <Count count={cartState.cartCount} />}
-        </div>
+         <div className="wishlist-info">
+            <FontAwesomeIcon
+              icon={faHeart}
+              color="var(--primary-color)"
+              size="xl"
+              onClick={() => Navigate(`/wishlist`)}
+            />
+            <div className="wishlist-count">
+              {wishlistState.wishlistCount > 0 && (
+                <div className="count-num">{wishlistState.wishlistCount}</div>
+              )}
+            </div>
+          </div>
+          <div className="cart-info">
+            <FontAwesomeIcon
+              icon={faCartShopping}
+              color="var(--primary-color)"
+              size="lg"
+              onClick={() => Navigate("/cart")}
+              
+            />
+            <div className="cart-count">
+              {" "}
+              {cartState.cartCount > 0 && <div className="count-num">{cartState.cartCount}</div>}
+            </div>
+          </div>
         <FontAwesomeIcon
           icon={faUser}
           size="xl"
-          color="black"
+          color="var(--Contrast-color)"
           onClick={() => Navigate("/profile")}
         />
       </div>
-     {navToggle? <div className="Nav-btns">
-        {isLogged ? (
-          <button className="btn-login" onClick={handleLogout}>
-            Logout
-          </button>
-        ) : (
-          <button className="btn-login">
-            <Link
-              to="/login"
-              style={{
-                textDecoration: "none",
-                backgroundColor: "transparent",
-                color: "brown",
-              }}
-            >
-              Login
-            </Link>
-          </button>
-        )}
-        <FontAwesomeIcon
-          icon={faHeart}
-          color="var(--primary-color)"
-          size="xl"
-          onClick={() => Navigate(`/wishlist`)}
-        />
-        {wishlistState.wishlistCount > 0 && (
-          <Count count={wishlistState.wishlistCount} />
-        )}
-        <FontAwesomeIcon
-          icon={faCartShopping}
-          color="var(--primary-color)"
-          size="lg"
-          onClick={() => Navigate("/cart")}
-        />
-        <div className="cart-count">
-          {" "}
-          {cartState.cartCount > 0 && <Count count={cartState.cartCount} />}
+      {navToggle ? (
+        <div className="Nav-btns">
+          {isLogged ? (
+            <button className="btn-login" onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <button className="btn-login">
+              <Link
+                to="/login"
+                style={{
+                  textDecoration: "none",
+                  backgroundColor: "transparent",
+                  color: "brown",
+                }}
+              >
+                Login
+              </Link>
+            </button>
+          )}
+          <div className="wishlist-info">
+            <FontAwesomeIcon
+              icon={faHeart}
+              color="var(--primary-color)"
+              size="xl"
+              onClick={() => Navigate(`/wishlist`)}
+            />
+            <div className="wishlist-count">
+              {wishlistState.wishlistCount > 0 && (
+                <div className="count-num">{wishlistState.wishlistCount}</div>
+              )}
+            </div>
+          </div>
+          <div className="cart-info">
+            <FontAwesomeIcon
+              icon={faCartShopping}
+              color="var(--primary-color)"
+              size="lg"
+              onClick={() => Navigate("/cart")}
+              
+            />
+            <div className="cart-count">
+              {" "}
+              {cartState.cartCount > 0 && <div className="count-num">{cartState.cartCount}</div>}
+            </div>
+          </div>
+          <FontAwesomeIcon
+            icon={faUser}
+            size="xl"
+            color="black"
+            onClick={() => Navigate("/profile")}
+          />
         </div>
-        <FontAwesomeIcon
-          icon={faUser}
-          size="xl"
-          color="black"
-          onClick={() => Navigate("/profile")}
-        />
-      </div>:""}
-      <span className="filter-option-mobile" onClick={handleFilterAndNavigation}>
-        { navToggle?<FontAwesomeIcon icon={faBarsStaggered} color="black" size="lg"/>:<FontAwesomeIcon icon={faBars} color="black" size="lg"/>}
+      ) : (
+        ""
+      )}
+      <span
+        className="filter-option-mobile"
+        onClick={handleFilterAndNavigation}
+      >
+        {navToggle ? (
+          <FontAwesomeIcon icon={faBarsStaggered} color="black" size="lg" />
+        ) : (
+          <FontAwesomeIcon icon={faBars} color="black" size="lg" />
+        )}
       </span>
     </div>
   );
